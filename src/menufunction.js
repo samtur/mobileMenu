788df25e28ad9451c8b48d28a8a19f284212d469
samtur <108389765+samtur@users.noreply.github.com>
2023-06-19 11:39:05 +0100

    started animating menu

--- src/menufunction.js
+++ src/menufunction.js
@@ -3,6 +3,12 @@ const menu = () => {
   const menuBtn = document.querySelector("#plusbtn");
 
   menuBtn.addEventListener("click", function () {
+    if (menuBtn.classList.contains("active")) {
+      menuBtn.classList.remove("active");
+    } else {
+      menuBtn.classList.add("active");
+    }
+
     if (menu.classList.contains("hidden")) {
       menu.classList.remove("hidden");
     } else {
